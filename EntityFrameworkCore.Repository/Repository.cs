71b6739031efbb9e6610f763787189ee0e12e1a7
Adam Hancock <adam.hancock@solentim.com>
2018-01-31 12:21:16 +0000

    Added nuget publish

--- EntityFrameworkCore.Repository/Repository.cs
+++ EntityFrameworkCore.Repository/Repository.cs
@@ -121,4 +121,66 @@ namespace EntityFrameworkCore.Repository
             return items;
         }
     }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class ApplicationDataContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    }
+
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetPersons(Func<Person, bool> predicate);
+        bool CreatePerson(Person person);
+        bool UpdatePerson(Person person);
+        bool DeletePerson(Person person);
+    }
+
+    public class PersonRepository : Repository<ApplicationDataContext>, IPersonRepository
+    {
+        public IEnumerable<Person> GetPersons(Func<Person, bool> predicate)
+        {
+            return All(predicate);
+        }
+
+        public bool CreatePerson(Person person)
+        {
+            return Create(person);
+        }
+
+        public bool UpdatePerson(Person person)
+        {
+            return Update(person);
+        }
+
+        public bool DeletePerson(Person person)
+        {
+            return Delete(person);
+        }
+    }
+
+    public interface IPersonService
+    {
+        IEnumerable<Person> GetPersonsByLastName(string lastname);
+    }
+
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonService(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Person> GetPersonsByLastName(string lastname)
+        {
+            return _repository.GetPersons(p => p.LastName.Equals(lastname));
+        }
+    }
 }
